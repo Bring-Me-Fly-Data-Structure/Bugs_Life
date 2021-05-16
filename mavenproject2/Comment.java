d73ab083d3d6b6b6f44a7f18f03527a0ccac9696
richiephang <76896343+richiephang@users.noreply.github.com>
2021-05-17 00:04:39 +0800

    Comment class timestamp fix

--- mavenproject2/Comment.java
+++ mavenproject2/Comment.java
@@ -1,7 +1,7 @@
 
 package com.mycompany.mavenproject2;
 import java.util.List;
-//import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
@@ -22,12 +22,40 @@ public class Comment {
     @JsonProperty("text")
     private String text;
     @JsonProperty("react")
-    private List<React> react = null;
+    private List<React> react ;
     @JsonProperty("timestamp")
     private Integer timestamp;
     @JsonProperty("user")
     private String user;
 
+    @JsonIgnore
+    private java.util.Date timestampformat;
+    
+    public Comment() {
+    }
+
+    @JsonCreator
+    //constructor for data.json 
+    public Comment(@JsonProperty("comment_id") Integer commentId, @JsonProperty("text") String text, @JsonProperty("react") List<React> react, @JsonProperty("timestamp") Integer timestamp, @JsonProperty("user") String user) {
+        this.commentId = commentId;
+        this.text = text;
+        this.react = react;
+        this.timestamp = timestamp;
+        this.timestampformat = new java.util.Date((long) timestamp * 1000);
+        this.user = user;
+
+    }
+
+    //constructor for user input
+    public Comment(String text, String user) {
+        this.text = text;
+        this.user = user;
+        this.timestampformat = new java.util.Date();
+        Integer i = Math.toIntExact(new java.util.Date().getTime() / 1000);
+        this.timestamp = i;
+    }
+    
+    
     @JsonProperty("comment_id")
     public Integer getCommentId() {
         return commentId;
@@ -77,11 +105,25 @@ public class Comment {
     public void setUser(String user) {
         this.user = user;
     }
+    
+    public Date getTimestampformat() {
+        return timestampformat;
+    }
 
-    //need to modify to become same as example
+    public void setTimestampformat(Date timestampformat) {
+        this.timestampformat = timestampformat;
+    }
+    
+    public String changeDateFormat() {
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy/MM/dd hh:mm");
+        return ft.format(getTimestampformat());
+    }
+
     @Override
     public String toString() {
-        return "Comment{" + "commentId=" + commentId + ", text=" + text + ", react=" + react + ", timestamp=" + timestamp + ", user=" + user + '}';
+        String result = "#" + commentId + "     Created on: " + changeDateFormat() + "    By: " + user + "\n" + text + "\n$$ " + react + "\n";
+        // return "Comment{" + "commentId=" + commentId + ", text=" + text + ", react=" + react + ", timestamp=" + timestamp + ", user=" + user + '}';
+        return result;
     }
 
     
