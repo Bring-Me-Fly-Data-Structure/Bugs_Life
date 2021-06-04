b0f9cf055bcc330bab40fcd6895de36780f08a76
richiephang <76896343+richiephang@users.noreply.github.com>
2021-06-04 15:06:04 +0800

    fix index out of bound error when reacting

--- mavenproject3/React2.java
+++ mavenproject3/React2.java
@@ -1,67 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mavenproject3;
 
+package mavenproject3;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import javax.persistence.*;
-
+import javax.transaction.Transactional;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "reaction",
     "count"
 })
-
+@Transactional
 @Entity
 @Table(name = "reaction")
 public class React2 implements Serializable {
-     @JsonIgnore    // Create an EntityManagerFactory when you start the application
+
+    @JsonIgnore    // Create an EntityManagerFactory when you start the application
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
             .createEntityManagerFactory("hibernateTest");
-    
+
     @JsonIgnore
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "react_id")
     @JsonIgnore
     private int react_id;
-    
+
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "comment_id", nullable = false)
-    private Comment comment;  
-    
-    @Column(name = "reaction")     
+    private Comment comment;
+
+    @Column(name = "reaction")
     @JsonProperty("reaction")
     private String reaction;
-    
-    @Column(name = "count")      
+
+    @Column(name = "count")
     @JsonProperty("count")
     private Integer count;
 
     public React2() {
     }
 
-    public React2(String reaction, Integer count) {
+    @JsonCreator
+    public React2(@JsonProperty("reaction") String reaction, @JsonProperty("count") Integer count) {
         this.reaction = reaction;
         this.count = count;
     }
@@ -102,11 +96,6 @@ public class React2 implements Serializable {
         this.comment = comment;
     }
 
-//    public void reaction(String react,int commentID){
-//        //read json file
-//        //find comment id
-//        //write reaction... >> json file
-//    }
     @Override
     public String toString() {
 
@@ -148,61 +137,46 @@ public class React2 implements Serializable {
                 reactionIndex = 6;
                 break;
         }
-//        ObjectMapper mapper = new ObjectMapper();
-//        //read file
-//        Example root = mapper.readValue(new File("C:\\Users\\richi\\Desktop\\UM folder\\Y1S2\\WIA1002 DS\\assignment\\localDatabase\\final.json"), Example.class);
-//        //Update value in object
-//        int count = root.getProjects().get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex).getCount();
-//        root.getProjects().get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex).setCount(count + 1);
-//        String json = mapper.writeValueAsString(root);
-//
-//        try (FileWriter file = new FileWriter("C:\\Users\\richi\\Desktop\\UM folder\\Y1S2\\WIA1002 DS\\assignment\\localDatabase\\final.json")) {
-//
-//            file.write(json);
-//            System.out.println("Successfully updated json object to file...!!");
-//        }
 
-       EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         EntityTransaction et = null;
 
         // the lowercase c refers to the object
         // :custID is a parameterized query thats value is set below
         String strQuery = "SELECT c FROM Project c WHERE c.id IS NOT NULL";
+        String strQuery2 = "SELECT c FROM Issue c WHERE c.id IS NOT NULL";
 
         // Issue the query and get a matching Customer
         TypedQuery<Project> tq = em.createQuery(strQuery, Project.class);
+        TypedQuery<Issue> tq2 = em.createQuery(strQuery2, Issue.class);
         List<Project> projectList = new ArrayList<>();
+        List<Issue> issueList = new ArrayList<>();
 
         try {
             // Get matching customer object and output
             projectList = tq.getResultList();
-          //  System.out.println(projectList.get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact());
-            
+            issueList = tq2.getResultList();
+            //  System.out.println(projectList.get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact());
 
         } catch (NoResultException ex) {
             ex.printStackTrace();
         } finally {
-           // em.close();
+            // em.close();
         }
-        
-        
-    	React2 r = new React2();
- 
+
+        React2 r = new React2();
+
         try {
             // Get transaction and start
             et = em.getTransaction();
             et.begin();
- 
-            int count = projectList.get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex).getCount();
-        //    projectList.get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex).setCount(count + 1);
 
-        r = projectList.get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex);
-       // r = em.find(React.class,r.react_id );    
-//            System.out.println("*****");
-//            System.out.println(r.react_id.getReaction());
-        r.setCount(count+1);
-      //  r.setComment(projectList.get(Project.getProjectID() - 1).getIssues().get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1));
-            // Save the customer object
+            int count = issueList.get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex).getCount();
+            r = issueList.get(Issue.getIssueID() - 1).getComments().get(Comment.getCommentID() - 1).getReact().get(reactionIndex);
+
+            r.setCount(count + 1);
+
             em.persist(r);
 
             et.commit();
@@ -214,11 +188,8 @@ public class React2 implements Serializable {
             System.out.println("react sql error");
             ex.printStackTrace();
         } finally {
-            // Close EntityManager
-            System.out.println("react succesful?");
             em.close();
         }
     }
-
 
 }
