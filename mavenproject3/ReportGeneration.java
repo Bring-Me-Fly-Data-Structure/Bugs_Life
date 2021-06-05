2299f3e898b45618f5987b810837ce5fba195602
richiephang <76896343+richiephang@users.noreply.github.com>
2021-06-06 02:01:56 +0800

    Update ReportGeneration.java

--- mavenproject3/ReportGeneration.java
+++ mavenproject3/ReportGeneration.java
@@ -5,16 +5,11 @@
  */
 package mavenproject3;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
 import java.io.IOException;
-import static java.nio.file.Files.list;
-import static java.rmi.Naming.list;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
-import static java.util.Collections.list;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -31,8 +26,10 @@ import javax.persistence.TypedQuery;
  * @author richi
  */
 public class ReportGeneration {
+
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
             .createEntityManagerFactory("hibernateTest");
+
     public static void main(String[] args) throws IOException {
         long numResolved = 0;
         long numInProgress = 0;
@@ -42,7 +39,7 @@ public class ReportGeneration {
 //        ObjectMapper mapper = new ObjectMapper();
 //        //read file
 //        Example root = mapper.readValue(new File("C:\\Users\\richi\\Desktop\\UM folder\\Y1S2\\WIA1002 DS\\assignment\\localDatabase\\final.json"), Example.class);
-                EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
 
         // the lowercase c refers to the object
         // :custID is a parameterized query thats value is set below
@@ -58,85 +55,82 @@ public class ReportGeneration {
             // Get matching customer object and output
             projectList = tq.getResultList();
             listU = tq2.getResultList();
-            //testing
-//            System.out.println("***********");
-//            System.out.println(projectList.get(0).getIssues().get(0).getComments().get(0).getReact());
+            ArrayList<String> usersName = new ArrayList<>();
+            ArrayList<String> outer = new ArrayList<>();
+            //store user name as a list
+            for (int i = 0; i < listU.size(); i++) {
+                usersName.add(listU.get(i).getUsername());
+            }
+
+            //array of number of issue solved, each index here is same with username arraylist index
+            long[] numSolvedbyAssignee = new long[usersName.size()];
+
+            for (int i = 0; i < projectList.size(); i++) {
+                numResolved += findWeeklyStatus(projectList.get(i).getIssues(), "Resolved");
+                numInProgress += findWeeklyStatus(projectList.get(i).getIssues(), "In Progress");
+                numOpen += findWeeklyStatus(projectList.get(i).getIssues(), "Open");
+                findTag(outer, projectList.get(i).getIssues());
+
+                //loop through each issue, find number of issue solved by each user, store it in array 
+                for (int j = 0; j < usersName.size(); j++) {
+                    numSolvedbyAssignee[j] = findTopPerformer(projectList.get(i).getIssues(), usersName.get(j), "Resolved");
+                }
+            }
+
+            //find the index of highest solved , then the use the index to determine the top performer
+            long max = numSolvedbyAssignee[0];
+            int maxIndex = 0;
+            for (int i = 1; i < numSolvedbyAssignee.length; i++) {
+                if (numSolvedbyAssignee[i] > max) {
+                    max = numSolvedbyAssignee[i];
+                    maxIndex = i;
+                }
+            }
+
+            //report output
+            Calendar c = Calendar.getInstance();
+            c.setFirstDayOfWeek(Calendar.MONDAY);
+            c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+            c.set(Calendar.HOUR_OF_DAY, 0);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            c.set(Calendar.MILLISECOND, 0);
+            Date monday = c.getTime();
+            long numUnresolved = numOpen + numInProgress;
+            System.out.println("***************Weekly Report***************");
+            System.out.println("From " + changeDateFormat(monday) + " to " + changeDateFormat(new Date()) + "\n*******************************************");
+            System.out.println("Number of resolved issues: " + numResolved);
+            System.out.println("Number of In Progress issues: " + numInProgress);
+            System.out.println("Number of unresolved issues: " + numUnresolved);
+            if (max == 0) {
+                System.out.println("Top performer of the week: nobody");
+            } else {
+                System.out.println("Top performer of the week: " + usersName.get(maxIndex));
+            }
+            //   System.out.println(outer.toString());
+            if (outer.isEmpty()) {
+                System.out.println("Most frequent label of the week: no label found");
+            } else {
+                Map<String, Long> occurrences = outer.stream().collect(Collectors.groupingBy(w -> w, Collectors.counting()));
+                long maxKey = Collections.max(occurrences.values());
+                List<String> keys = new ArrayList<>();
+                for (Entry<String, Long> entry : occurrences.entrySet()) {
+                    if (entry.getValue() == maxKey) {
+                        keys.add(entry.getKey());
+                    }
+                }
+                System.out.println(occurrences);
+                System.out.print("Most frequent label of the week: ");
+                for (int i = 0; i < keys.size(); i++) {
+                    System.out.print("\"" + keys.get(i) + "\" ");
+                }
+            }
         } catch (NoResultException ex) {
             ex.printStackTrace();
         } finally {
             em.close();
         }
 
-        ArrayList<String> usersName = new ArrayList<>();
-        ArrayList<String> outer = new ArrayList<>();
-        //store user name as a list
-        for (int i = 0; i < listU.size(); i++) {
-            usersName.add(listU.get(i).getUsername());
-        }
-
-        //array of number of issue solved, each index here is same with username arraylist index
-        long[] numSolvedbyAssignee = new long[usersName.size()];
-
-        for (int i = 0; i < projectList.size(); i++) {
-            numResolved += findWeeklyStatus(projectList.get(i).getIssues(), "Resolved");
-            numInProgress += findWeeklyStatus(projectList.get(i).getIssues(), "In Progress");
-            numOpen += findWeeklyStatus(projectList.get(i).getIssues(), "Open");
-            findTag(outer, projectList.get(i).getIssues());
-
-            //loop through each issue, find number of issue solved by each user, store it in array 
-            for (int j = 0; j < usersName.size(); j++) {
-                numSolvedbyAssignee[j] = findTopPerformer(projectList.get(j).getIssues(), usersName.get(j), "Resolved");
-            }
-        }
-
-        //find the index of highest solved , then the use the index to determine the top performer
-        long max = numSolvedbyAssignee[0];
-        int maxIndex = 0;
-        for (int i = 1; i < numSolvedbyAssignee.length; i++) {
-            if (numSolvedbyAssignee[i] > max) {
-                max = numSolvedbyAssignee[i];
-                maxIndex = i;
-            }
-        }
-
-        //report output
-        Calendar c = Calendar.getInstance();
-        c.setFirstDayOfWeek(Calendar.MONDAY);
-        c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-        c.set(Calendar.HOUR_OF_DAY, 0);
-        c.set(Calendar.MINUTE, 0);
-        c.set(Calendar.SECOND, 0);
-        c.set(Calendar.MILLISECOND, 0);
-        Date monday = c.getTime();
-        long numUnresolved = numOpen + numInProgress;
-        System.out.println("***************Weekly Report***************");
-        System.out.println("From " + changeDateFormat(monday) + " to " + changeDateFormat(new Date()) + "\n*******************************************");
-        System.out.println("Number of resolved issues: " + numResolved);
-        System.out.println("Number of In Progress issues: " + numInProgress);
-        System.out.println("Number of unresolved issues: " + numUnresolved);
-        if (max == 0) {
-            System.out.println("Top performer of the week: nobody");
-        } else {
-            System.out.println("Top performer of the week: " + usersName.get(maxIndex));
-        }
-        //   System.out.println(outer.toString());
-        if (outer.isEmpty()) {
-            System.out.println("Most frequent label of the week: no label found");
-        } else {
-            Map<String, Long> occurrences = outer.stream().collect(Collectors.groupingBy(w -> w, Collectors.counting()));
-            long maxKey = Collections.max(occurrences.values());
-            List<String> keys = new ArrayList<>();
-            for (Entry<String, Long> entry : occurrences.entrySet()) {
-                if (entry.getValue() == maxKey) {
-                    keys.add(entry.getKey());
-                }
-            }
-            System.out.println(occurrences);
-            System.out.print("Most frequent label of the week: ");
-            for (int i = 0; i < keys.size(); i++) {
-                System.out.print("\"" + keys.get(i) + "\" ");
-            }
-        }
     }
 
     public static long findStatus(List<Issue> list, String status) {
