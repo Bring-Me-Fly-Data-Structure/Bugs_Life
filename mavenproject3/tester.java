24c291144f60356eb8c0c43d077db36b6bdd5cc4
richiephang <76896343+richiephang@users.noreply.github.com>
2021-06-08 16:19:00 +0800

    Update tester.java

--- mavenproject3/tester.java
+++ mavenproject3/tester.java
@@ -5,12 +5,8 @@
  */
 package mavenproject3;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -18,42 +14,75 @@ import java.util.Scanner;
  * @author richi
  */
 public class tester {
-    
-    public static void main(String[] args) throws IOException {
-       input();
-       Project.displayProject();
 
+    public static void main(String[] args) throws IOException {
+        homepage();
     }
 
-public static void input() throws IOException {
+    public static void homepage() throws IOException {
         Scanner in = new Scanner(System.in);
 
-        try {
-            System.out.println("Enter '1' to login, '2' to register");
-            int userInput = in.nextInt();
-            if (userInput == 1) {
-                User.login();
-            } else if (userInput == 2) {
-                User.register();
-                User.login();
-            } else if (userInput != 1 && userInput != 2) {
-                System.out.println("Invalid input. Please try again.");
-                System.out.println("");
-                input();
+        while (true) {
+
+            try {
+                System.out.println("Enter '1' to login, '2' to register");
+                int userInput = in.nextInt();
+                if (userInput == 1) {
+                    User.login();
+                    break;
+                } else if (userInput == 2) {
+                    User.register();
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid value!\n");
+                in.next();
             }
-            while (!User.isLogin_status()) {
-                System.out.println("Please login to proceed....");
-                User.login();
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please try again.");
-            System.out.println("");
-            input();
         }
 
+        while (!User.isLogin_status()) {
+            System.out.println("Please login to proceed....");
+            User.login();
+        }
+        if (User.getLoginName().equals("admin")) {
+            adminHomepage();
+        } else {
+            Project.displayProject();
+        }
     }
-    
-    
 
-    
+    public static void adminHomepage() throws IOException {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("\nEnter '1' to view access control list\nOr '2' to trace the abnormal activities\n'3' to generate report\n'4' to Import or Export JSON data\n'5' to view projects and issues");
+
+                int userInput = in.nextInt();
+                if (userInput == 1) {
+                    access_control.adminLogReport();
+                    adminHomepage();
+                    break;
+                } else if (userInput == 2) {
+                    adminlog.adminLogReport();
+                    adminHomepage();
+                    break;
+                } else if (userInput == 3) {
+                    ReportGeneration.generateReport();
+                    adminHomepage();
+                    break;
+                } else if (userInput == 4) {
+                    JSON_IMPORT_EXPORT.importExport();
+                    adminHomepage();
+                    break;
+                } else if (userInput == 5) {
+                    Project.displayProject();
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid value!\n");
+                in.next();
+            }
+        }
+    }
+
 }
