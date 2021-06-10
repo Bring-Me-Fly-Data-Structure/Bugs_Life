116bc218028b2c8ff36c2ea8c43abfc2d3a5ee8c
annjoeannjoe <76165509+annjoeannjoe@users.noreply.github.com>
2021-06-10 21:02:39 +0800

    Update tester.java

--- mavenproject3/tester.java
+++ mavenproject3/tester.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mavenproject3;
 
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- *
- * @author richi
- */
 public class tester {
 
     public static void main(String[] args) throws IOException {
