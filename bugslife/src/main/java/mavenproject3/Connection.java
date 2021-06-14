43dc965712277229ddd72c85ce725379d7e137b6
annjoeannjoe <76165509+annjoeannjoe@users.noreply.github.com>
2021-06-14 17:46:08 +0800

    Update Connection.java

--- bugslife/src/main/java/mavenproject3/Connection.java
+++ bugslife/src/main/java/mavenproject3/Connection.java
@@ -10,11 +10,6 @@ public class Connection {
         try {
             //    Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bugs_life", "root", "");
-            //Statement st = conn.createStatement();
-            /*
-            for debugging purpose, can be removed
-            System.out.println("Connected to database in ConnectionMySQL.getConnection()");
-             */
             return con;
         } catch (SQLException e) {
             System.out.println(e);
