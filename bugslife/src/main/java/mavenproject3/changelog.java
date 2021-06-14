ce86f3f206405fb6e19ef6cf357aeab8bc64907b
annjoeannjoe <76165509+annjoeannjoe@users.noreply.github.com>
2021-06-14 18:03:28 +0800

    Update changelog.java

--- bugslife/src/main/java/mavenproject3/changelog.java
+++ bugslife/src/main/java/mavenproject3/changelog.java
@@ -151,22 +151,10 @@ public class changelog {
             pS = changelogSQL.getConnection().prepareStatement(searchChangeLOG);
             result = pS.executeQuery();
             if(!result.next()==true){
-//            while (result.next()) {
-//                String edit_time = result.getString("edit_time");
-//                String edittor = result.getString("edittor");
-//                String projectName = result.getString("project_name");
-//                int projectID = result.getInt("project_id");
-//                String issueName = result.getString("issue_name");
-//                int issueID = result.getInt("issue_id");
-//                String detail = result.getString("detail");
-//
-//                System.out.println("Editted Time : " + edit_time + "\nEdittor : " + edittor + "\nProject Name : " + projectName + "   Project ID : " + projectID + "\nIssue Name : " + issueName + "   Issue ID : " + issueID + "\nDetail : \n" + detail);
-//                System.out.println("");
-//            }
-//            }else{
-                 System.out.println("********************");
+
+                System.out.println("********************");
                 System.out.println("No change be made");
-                 System.out.println("********************");
+                System.out.println("********************");
             }
             changelogSQL.getConnection().close();
         } catch (SQLException ex) {
