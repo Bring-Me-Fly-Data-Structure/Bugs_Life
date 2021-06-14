e2189c41977e277add0a97ebbc9c9acd55a85a40
richiephang <76896343+richiephang@users.noreply.github.com>
2021-06-14 22:05:21 +0800

    Update Issuequeue.java

--- bugslife/src/main/java/mavenproject3/Issuequeue.java
+++ bugslife/src/main/java/mavenproject3/Issuequeue.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Scanner;
